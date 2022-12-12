9d41aa7e4510e5045f3a98ecc718da40d4f9fcf5
Олександр Правосуд <a.a.prosud@gmail.com>
2022-12-12 17:07:07 +0200

    Added DbContext

---
+++ ThirdEye.Back/DataAccess/Contexts/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThirdEye.Back.DataAccess.Entities;
+
+namespace ThirdEye.Back.DataAccess.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<InsitutionWorker> Workers { get; set; }
+        public DbSet<Institution> Institutions { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomStateChange> RoomsStateHistories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
