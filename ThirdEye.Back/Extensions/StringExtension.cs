cf30146c140c9a60aec11af0255e64edc3f728a0
Олександр Правосуд <a.a.prosud@gmail.com>
2022-12-14 15:33:05 +0200

    Added string extensions for localization

---
+++ ThirdEye.Back/Extensions/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace ThirdEye.Back.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Using(this string @this, IStringLocalizer localizer) =>
+            localizer[@this].Value;
+
+        public static BadRequestObjectResult ToBadRequestUsing(this string @this, IStringLocalizer localizer) =>
+            new BadRequestObjectResult(new[] { @this.Using(localizer) });
+    }
+}
