ca5bc05e954b014b7ac140a86c955a5a169524f7
Олександр Правосуд <a.a.prosud@gmail.com>
2022-12-14 15:03:35 +0200

    Added base class for Authorized controllers

---
+++ ThirdEye.Back/Controllers/Abstratctions/AuthorizedControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using ThirdEye.Back.DataAccess.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThirdEye.Back.Controllers.Abstratctions
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public abstract class AuthorizedControllerBase : Controller
+    {
+        protected AuthorizedControllerBase()
+        {
+
+        }
+
+        protected UserManager<User> UserManager { get; set; }
+        protected async Task<User> GetUser() => await UserManager.GetUserAsync(User);
+        protected string GetUserId() => UserManager.GetUserId(User);
+    }
+}
